a63dbc5d646ae88c752cefbfff8413bddecb7f10
Theo <12211068+Whatisthisname@users.noreply.github.com>
2025-04-01 14:07:52 +0200

    before reorder loop break condition

--- test_bidiag.py
+++ test_bidiag.py
@@ -55,66 +55,64 @@ def bbbbidiagonalize(A, start_vector) -> BidiagOutput:
     )
 
 
-def bidiagonalize_jvp(primals, tangents) -> tuple[BidiagOutput, BidiagOutput]:
+def bidiagonalize_jvp(
+    primals, tangents, iterations: int
+) -> tuple[BidiagOutput, BidiagOutput]:
     A, start_vector = primals
     dA, d_start_vector = tangents
 
-    m = A.shape[0]
-    n = A.shape[1]
+    height = A.shape[0]
+    width_ = A.shape[1]
 
-    any_n_vec = np.zeros(n)
-    any_m_vec = np.zeros(m)
-    any_number = 0
+    zero_n_vec = np.zeros(width_)
+    zero_m_vec = np.zeros(height)
     bs = [0]
-    as_ = [any_number]
+    as_ = [0]
 
     c = 1 / np.linalg.norm(start_vector)
-    rs = [any_n_vec, start_vector * c]
-    ls = [any_m_vec]
+    rs = [zero_n_vec, start_vector * c]
+    ls = [zero_m_vec]
 
-    terminated_naturally = False
-    for k in range(1, max(n, m) + 1):
-        t = A @ rs[k] - bs[k - 1] * ls[k - 1]
-        alpha_k = np.linalg.norm(t)
-        as_.append(alpha_k)
-        l_k = t / alpha_k
-        ls.append(l_k)
+    for n in range(1, iterations + 1):
+        t = A @ rs[n] - bs[n - 1] * ls[n - 1]
+        as_.append(np.linalg.norm(t))
+        ls.append(t / as_[n])
 
-        w = A.T @ l_k - alpha_k * rs[k]
+        w = A.T @ ls[n] - as_[n] * rs[n]
         beta_k = np.linalg.norm(w)
-        bs.append(beta_k)
-
-        if np.allclose(beta_k, 0, atol=1e-10) or np.isnan(beta_k):
-            terminated_naturally = True
+        if np.allclose(beta_k, 0, atol=1e-7) or np.isnan(beta_k):
             break
 
+        bs.append(beta_k)
         r_kp1 = w / beta_k
         rs.append(r_kp1)
 
+    print("as")
+    print(np.array(as_[1:]).round(4))
+    print("bs")
+    print(np.array(bs[1:]).round(4))
+
     L = np.array(ls[1:]).T
-    if terminated_naturally:
-        R = np.array(rs[1:]).T
-    else:
-        R = np.array(rs[1:-1]).T
-    B = np.diag(as_[1:]) + np.diag(bs[1:-1], k=1)
+    R = np.array(rs[1:]).T
+    B = np.diag(as_[1:]) + np.diag(bs[1:], k=1)
 
     primal_output = BidiagOutput(rs=rs, ls=ls, L=L, B=B, R=R, alphas=as_, betas=bs, c=c)
 
     d_as = [0] * len(as_)
     d_bs = [0] * len(bs)
-    d_rs = [any_n_vec.copy() * 0 for _ in range(len(rs))]
+    d_rs = [zero_n_vec.copy() * 0 for _ in range(len(rs))]
     d_rs[1] = (
         d_start_vector - start_vector * (start_vector.T @ d_start_vector)
     ) / np.linalg.norm(start_vector)
-    d_ls = [any_m_vec.copy() * 0 for _ in range(len(ls))]
+    d_ls = [zero_m_vec.copy() * 0 for _ in range(len(ls))]
 
     # d_rs[1] = d_start_vector, known
     # d_ls[0] = doesn't matter because bs_[0] = 0
     # d_bs[0] = 0
 
     # In each iteration, assume we already know d_rs[n], d_ls[n-1], d_bs[n-1]
-    rank = len(as_) - 1
-    for n in range(1, len(as_)):
+    rank = len(as_[1:])
+    for n in range(1, len(as_[1:]) + 1):
         d_a_n = ls[n].T @ (A @ d_rs[n] + dA @ rs[n] - d_ls[n - 1] * bs[n - 1])
         d_as[n] = d_a_n
         d_l_n = (
@@ -153,7 +151,7 @@ def bidiagonalize_jvp(primals, tangents) -> tuple[BidiagOutput, BidiagOutput]:
 
     dL = np.array(d_ls[1:]).T
     dR = np.array(d_rs[1:-1]).T
-    dB = np.diag(d_as[1:]) + np.diag(d_bs[1:-1], k=1)  # beta index -1?
+    dB = np.diag(d_as[1:]) + np.diag(d_bs[1:], k=1)  # beta index -1?
 
     tangent_output = BidiagOutput(
         rs=d_rs, ls=d_ls, L=dL, B=dB, R=dR, alphas=d_as, betas=d_bs, c=d_c
@@ -177,30 +175,39 @@ def test_bidiag_jvp(seed):
     d_start_vector = np.random.randn(m)
 
     result, tangents = bidiagonalize_jvp(
-        primals=(A, start_vector), tangents=(d_A, d_start_vector)
+        primals=(A, start_vector),
+        tangents=(d_A, d_start_vector),
+        iterations=20,
     )
 
     h = 0.000001
     result_wiggled, tangents_ = bidiagonalize_jvp(
         primals=(A + d_A * h, start_vector + d_start_vector * h),
         tangents=(d_A, d_start_vector),  # doesn't matter
+        iterations=20,
     )
 
     assert np.allclose((result_wiggled.c - result.c) / h, tangents.c, atol=1e-2)
 
+    print(result.R)
+
     for idx in range(1, len(result.rs)):
         for field in ["rs", "alphas", "ls", "betas"]:
-            print("-- Field: ", field, f"[{idx}]", sep="")
+            print(f"-- Field: {field}[{idx}]".ljust(20), sep="", end="")
+            try:
+                aprox = (
+                    result_wiggled.__getattribute__(field)[idx]
+                    - result.__getattribute__(field)[idx]
+                ) / h
 
-            aprox = (
-                result_wiggled.__getattribute__(field)[idx]
-                - result.__getattribute__(field)[idx]
-            ) / h
-
-            exact = tangents.__getattribute__(field)[idx]
-            assert np.allclose(
-                aprox, exact, atol=1e-2
-            ), f"\nApprox: {aprox}, \nExact: {exact}"
+                exact = tangents.__getattribute__(field)[idx]
+                assert np.allclose(
+                    aprox, exact, atol=1e-2
+                ), f"\nApprox: {aprox}, \nExact: {exact}"
+                print(" (OK)")
+            except IndexError:
+                print(" (IndexError)")
+                continue
 
 
 @pytest.mark.parametrize("seed", range(20))
@@ -211,7 +218,7 @@ def test_bidiag_tall_matrix(seed):
     A = np.random.randn(n, m)  # random tall-or-square matrix
     start_vector = 2 * np.eye(1, m).flatten()
 
-    result, _ = bidiagonalize_jvp((A, start_vector), (A, start_vector))
+    result, _ = bidiagonalize_jvp((A, start_vector), (A, start_vector), iterations=20)
 
     print(A)
     print()
@@ -236,13 +243,14 @@ def test_bidiag_tall_matrix(seed):
     ), "A.T L != R B.T + extra"
 
 
-def _test_bidiag_wide_matrix():
+def _test_bidiag_wide_matrix(seed: int):
+    np.random.seed(seed)
     n = 2
     m = 4
     A = np.random.randn(n, m)  # random 2x4 (wide) matrix
     start_vector = 2 * np.eye(1, m).flatten()
 
-    result, _ = bidiagonalize_jvp((A, start_vector), (A, start_vector))
+    result, _ = bidiagonalize_jvp((A, start_vector), (A, start_vector), iterations=20)
 
     print(A)
     print()
@@ -267,7 +275,41 @@ def _test_bidiag_wide_matrix():
     ), "A.T L != R B.T + extra"
 
 
-# # test_bidiag_wide_matrix()
+def _test_wide_matrix(seed: int):
+    np.random.seed(seed)
+    m = 4  # , np.random.randint(low=2, high=4 + 1)
+    n = np.random.randint(low=2, high=m + 1)
+    A = np.random.randn(n, m)  # random tall-or-square matrix
+    print("A shape:", A.shape)
+    start_vector = 2 * np.eye(1, m).flatten()
+
+    result, _ = bidiagonalize_jvp((A, start_vector), (A, start_vector), iterations=20)
+
+    print(A)
+    print()
+    print(result.L @ result.B @ result.R.T)
+
+    assert np.allclose(result.L @ result.B @ result.R.T, A, atol=1e-3), "A != LBR^T"
+
+    # Inspect reduced iteration count properties:
+    r = np.random.randint(1, min(m, n))
+
+    L = result.L[:, :r]
+    B = result.B[:r, :r]
+    R = result.R[:, :r]
+
+    assert np.allclose(L.T @ L, np.eye(r), atol=1e-5), "L^TL is not identity"
+    assert np.allclose(R.T @ R, np.eye(r), atol=1e-5), "R^TR is not identity"
+    assert np.allclose(A @ R, L @ B, atol=1e-5), "AR != LB"
+    assert np.allclose(
+        A.T @ L,
+        R @ B.T + np.outer(result.betas[r] * result.rs[r + 1], np.eye(1, r, k=r - 1)),
+        atol=1e-5,
+    ), "A.T L != R B.T + extra"
+
+
+# test_bidiag_jvp(1)
+# _test_bidiag_wide_matrix(1)
+_test_wide_matrix(1)
 # test_bidiag_tall_matrix(1)
-# test_bidiag_tall_matrix(2)
-# test_bidiag_tall_matrix(3)
+# [test_bidiag_tall_matrix(i) for i in range(120)]
